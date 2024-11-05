d0591c56ce426de31314029ef5f730613d244d8e
ozzx <ozzx@kali.mhmd>
2024-11-05 19:38:35 +0200

    pending

--- src/components/common/shows/ShowsList/ShowsList.jsx
+++ src/components/common/shows/ShowsList/ShowsList.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
+import { PaginationWrapper, ShowsListWrapper } from "./ShowsList.styles";
+import SectionTitle from "../../SectionTitle/SectionTitle";
+import { Container } from "../../../../styles/global/default";
+import ShowsItem from "../ShowsItem/ShowsItem";
 
-function ShowsList({showsData,showsTitle}) {
+function ShowsList({ showsData, showsTitle }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const showsPerPage = 40;
+  const indexOfLastShow = currentPage * showsPerPage;
+  const indexOfFirstShow = indexOfLastShow - showsPerPage;
+  const currentShows = showsData.slice(indexOfFirstShow, indexOfLastShow);
+  const totalPages = Math.ceil(showsData.length / showsPerPage)
+
   return (
-    <ShowsList>
-      
-    </ShowsList>
-  )
+    <ShowsListWrapper>
+      <SectionTitle title={showsTitle} />
+      <Container>
+        <div className="shows-list grid">
+          {
+            currentShows?.map((show)=>{
+              <ShowsItem key={show.id} itemData={show} />
+            })
+          }
+        </div>
+        <PaginationWrapper className="flex items-center justify-center"></PaginationWrapper>
+      </Container>
+    </ShowsListWrapper>
+  );
 }
 
-export default ShowsList
+export default ShowsList;
+
